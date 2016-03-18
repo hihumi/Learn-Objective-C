b15c251392e3a7a7fc5010ff1120f4992482f67a
hihumi <sth@sth-no-MacBook-ProProPro.local>
2016-03-19 01:01:59 +0900

    add file

---
+++ basis/variable01.m
@@ -0,0 +1,12 @@
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool {
+        int num;
+        num = 100;
+        NSLog(@"num is %d\n", num);
+    }
+
+    return 0;
+}
